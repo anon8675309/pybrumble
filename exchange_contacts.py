87a89d6b66100584de64c7296097f1675720feb8
anon8675309 <anon8675309@github.com>
2020-02-02 20:53:54 -0600

    Refactored to be more reusable and added some documentation

--- exchange_contacts.py
+++ exchange_contacts.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+This program will generate an ephemeral keypair, as described in section 2 of the BQP specification.
+
+Example usage:
+./exchange_contacts.py
+"""
 from argparse import ArgumentParser
 from base64 import b64encode, b64decode
 from bdf import encode_data, extract_data, bind_to_lan, open_connection, \
@@ -19,23 +25,22 @@ try:
     from pyqrcode import create
     import png  # Used by pyqrcode to output image
 except ImportError as e:
-    print("%s" % e)
-    print("To install dependencies: pip3 install pure25519 blake256 pyqrcode pypng")
+    info("%s" % e)
+    info("To install dependencies: pip3 install pure25519 blake256 pyqrcode pypng")
     from sys import exit
     exit(1)
-
 
 
 DEFAULT_QR_CODE_FILENAME="contact_exchange.png"
 DEFAULT_PRIVATE_KEY_FILE="ephemeral"
 DEFAULT_PUBLIC_KEY_FILE="ephemeral.pub"
-"""
-This program will generate an ephemeral keypair, as described in section 2 of the BQP specification.
 
-Example usage:
-./exchange_contacts.py
-"""
-if __name__ == "__main__":
+def get_arg_parser():
+    """
+    Builds program arguments.
+    :returns: Parser which will deal with command line arguments
+    :rtype: `class:ArgumentParser`
+    """
     parser = ArgumentParser(description='Generates ephemerial keys (if they do not exist) and does a key exchange')
     parser.add_argument("--private-key-file", default=DEFAULT_PRIVATE_KEY_FILE,
                     help=("Name/location of private key file (Default: %s)" % DEFAULT_PRIVATE_KEY_FILE))
@@ -50,8 +55,49 @@ if __name__ == "__main__":
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-v", "--verbose", action="count", help=('Verbose output (for debugging issues)'))
     group.add_argument("-q", "--quiet", action="count", help=('Quiet down the output'))
-    args = parser.parse_args()
+    return parser
 
+def write_barcode(pub, bluetooth, lan):
+    """
+    Creates a QR code to be scanned from a public key and transport types.
+    :param pub: public key
+    :type pub: binary string
+    :param bluetooth: true if Bluetooth is an available transport
+    :type bluetooth: boolean
+    :param lan: true if LAN is an available transport
+    :type lan: boolean
+    :returns: QR code that the other peer should scan
+    :rtype: ?
+    """
+    commitment = create_commitment(pub.to_bytes())
+    my_scan_payload = gen_scan_payload(commitment, bluetooth, lan)
+    b64_encoded_payload = b64_encode(encode_data(my_scan_payload))
+
+    info(b64_encoded_payload.decode())
+    return create(b64_encoded_payload)
+
+def recv_key_from_lan(conn):
+    """
+    :param conn: Connection to peer
+    :type conn: `class:Socket`
+    :returns: public key
+    :rtype: binary string?
+    """
+    record = recv_record(conn)
+    if record[1] == RECORD_TYPE_KEY:
+        pub = record[2]
+        # If the key doesn't match the commitment, we must abort
+        if other_scan_payload[1] != create_commitment(pub):
+            send_record(conn, RECORD_TYPE_ABORT, b"")
+            raise Exception("Public key did not match commitment!")
+    else:
+        raise Exception("Unexpected record type received: {}".format(record[1]))
+    return pub
+
+
+if __name__ == "__main__":
+    parser = get_arg_parser()
+    args = parser.parse_args()
     setup_logging(args)
 
     try:
@@ -64,19 +110,13 @@ if __name__ == "__main__":
         debug("Saving keys to disk...")
         save_keys(priv, pub, args.private_key_file, args.public_key_file)
 
-    commitment = create_commitment(pub.to_bytes())
-    my_scan_payload = gen_scan_payload(commitment, args.bluetooth, args.lan)
-
-    wire_encoded_payload = encode_data(my_scan_payload)
-    b64_encoded_payload = b64encode(wire_encoded_payload)
-    print(b64_encoded_payload.decode())
-    qrcode = create(b64_encoded_payload)
+    qrcode = write_barcode(pub, args.bluetooth, args.lan)
     qrcode.png(args.output, scale=8)
 
     # Read in the other person's scan_payload from stdin
     binary_other_scan_payload = b64decode(stdin.readline())
     other_scan_payload, reamining_data = extract_data(binary_other_scan_payload)
-    print(repr(other_scan_payload))
+    info(repr(other_scan_payload))
 
     # Now we need to make sure we're bound on the IP/port we said we'd be listening on
     ip, port = args.lan.split(":")
@@ -84,7 +124,7 @@ if __name__ == "__main__":
 
     # Per section 3 of the bqp spec, we determine whether we are ALICE or BOB
     i_am_alice = commitment < other_scan_payload[1]
-    print("I am %s" % ("Bob", "Alice")[i_am_alice])
+    info("I am %s" % ("Bob", "Alice")[i_am_alice])
     # The next phase of the key exchange is done online, which is described in section 4 of the bqp spec
     if i_am_alice:
         s.close()  # Close my connection, I want to use Bob's connection
@@ -92,48 +132,36 @@ if __name__ == "__main__":
         debug("Connecting to Bob on port %d" % remote_port)
         conn = open_connection(remote_ip, remote_port)
         send_record(conn, RECORD_TYPE_KEY, pub.to_bytes())
+        pub_b = recv_key_from_lan(conn)
+        shared_secret = calculate_shared_secret_alice(priv, pub.to_bytes(), pub_b)
+        debug("shared secret = %s" % shared_secret)
+        # Alice sends her confirmation code, and then receives and checks Bob's
+        send_record(conn, RECORD_TYPE_CONFIRM, gen_confirmation_alice(shared_secret,
+                wire_encoded_payload, pub.to_bytes(), binary_other_scan_payload, pub_b))
         record = recv_record(conn)
-        if record[1] == RECORD_TYPE_KEY:
-            pub_b = record[2]
-            # If bob's key doesn't match his commitment, we must abort
-            if other_scan_payload[1] != create_commitment(pub_b):
+        if record[1] == RECORD_TYPE_CONFIRM:
+            generated_confirmation = gen_confirmation_bob(shared_secret,
+                wire_encoded_payload, pub.to_bytes(), binary_other_scan_payload, pub_b)
+            if record[2] != generated_confirmation:
                 send_record(conn, RECORD_TYPE_ABORT, b"")
-                raise Exception("Public key did not match commitment!")
-            shared_secret = calculate_shared_secret_alice(priv, pub.to_bytes(), pub_b)
-            debug("shared secret = %s" % shared_secret)
-            # Alice sends her confirmation code, and then receives and checks Bob's
-            send_record(conn, RECORD_TYPE_CONFIRM, gen_confirmation_alice(shared_secret,
-                    wire_encoded_payload, pub.to_bytes(), binary_other_scan_payload, pub_b))
-            record = recv_record(conn)
-            if record[1] == RECORD_TYPE_CONFIRM:
-                generated_confirmation = gen_confirmation_bob(shared_secret,
-                    wire_encoded_payload, pub.to_bytes(), binary_other_scan_payload, pub_b)
-                if record[2] != generated_confirmation:
-                    send_record(conn, RECORD_TYPE_ABORT, b"")
-                    raise Exception("Confirmation record did not match expeced value!")
+                raise Exception("Confirmation record did not match expeced value!")
     else:
         debug("Waiting for Alice to connect to us on port %s..." % port)
         conn, remote_addr = s.accept()
+        pub_a = recv_key_from_lan(conn)
+        send_record(conn, RECORD_TYPE_KEY, pub.to_bytes())
+        shared_secret = calculate_shared_secret_bob(priv, pub_a, pub.to_bytes())
+        debug("shared secret = %s" % shared_secret)
+        # Bob receives Alice's confirmation, verifies it, then sends his own
         record = recv_record(conn)
-        if record[1] == RECORD_TYPE_KEY:
-            pub_a = record[2]
-            # If the key doesn't match the commitment, we must abort
-            if other_scan_payload[1] != create_commitment(pub_a):
+        if record[1] == RECORD_TYPE_CONFIRM:
+            generated_confirmation = gen_confirmation_alice(shared_secret,
+                binary_other_scan_payload, pub_a, wire_encoded_payload, pub.to_bytes())
+            if record[2] != generated_confirmation:
                 send_record(conn, RECORD_TYPE_ABORT, b"")
-                raise Exception("Public key did not match commitment!")
-            send_record(conn, RECORD_TYPE_KEY, pub.to_bytes())
-            shared_secret = calculate_shared_secret_bob(priv, pub_a, pub.to_bytes())
-            debug("shared secret = %s" % shared_secret)
-            # Bob receives Alice's confirmation, verifies it, then sends his own
-            record = recv_record(conn)
-            if record[1] == RECORD_TYPE_CONFIRM:
-                generated_confirmation = gen_confirmation_alice(shared_secret,
-                    binary_other_scan_payload, pub_a, wire_encoded_payload, pub.to_bytes())
-                if record[2] != generated_confirmation:
-                    send_record(conn, RECORD_TYPE_ABORT, b"")
-                    raise Exception("Confirmation record did not match expeced value!")
-            send_record(conn, RECORD_TYPE_CONFIRM, gen_confirmation_bob(shared_secret,
-                    binary_other_scan_payload, pub_a, wire_encoded_payload, pub.to_bytes(), ))
+                raise Exception("Confirmation record did not match expeced value!")
+        send_record(conn, RECORD_TYPE_CONFIRM, gen_confirmation_bob(shared_secret,
+                binary_other_scan_payload, pub_a, wire_encoded_payload, pub.to_bytes(), ))
     master_key = KDF(shared_secret, [b"MASTER_KEY"])
     shared_secret = None
-    print("master_key = %s" % hexlify(master_key).decode())
+    info("master_key = %s" % hexlify(master_key).decode())
